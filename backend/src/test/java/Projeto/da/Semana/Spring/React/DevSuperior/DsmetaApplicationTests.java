039e8017609fcdcd0bc866d87eb2afb89467f38e
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-10 13:50:15 -0300

    Project create

---
+++ backend/src/test/java/Projeto/da/Semana/Spring/React/DevSuperior/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.da.Semana.Spring.React.DevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
